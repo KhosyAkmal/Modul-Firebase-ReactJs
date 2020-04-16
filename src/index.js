55a6cc93974e59b8d4e1b77ab7bc43fb4169da89
KhosyAkmal <akmalkhosy@gmail.com>
2020-04-16 13:44:38 +0700

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {userState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Switch, BrowserRouter as Router, Route} from "react-router-dom";
+import routes from "./routes.js";
+import Header from "./Header";
+import "./styles.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as firebase from "firebase";
+import firebaseConfig from "./firebase.config";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = userState(false);
+
+  return (
+    <AuthContext.Provider value ={{isLoggedIn, setLoggedIn}}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+
+          <Switch>
+            {routes.map (route => (
+              <Route
+              key = {route.key}
+              path = {route.path}
+              exact ={route.exact}
+              component ={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
