6c8dcb122a6a39ee93d8287d23664dafdefd6750
KhosyAkmal <akmalkhosy@gmail.com>
2020-04-16 13:45:47 +0700

    add join

---
+++ src/Join.js
@@ -0,0 +1,59 @@
+import React, {userState, userContext} from "react";
+import { AuthContext } from "./index";
+import * as firebase from "firebase";
+
+const Join = () => {
+    const [email, setEmail] = userState("");
+    const [password, setPassword] = userState("");
+    const [error, setErrors] = userState("");
+
+    const Auth = userContext(AuthContext);
+    const handleForm = e => {
+        e.preventDefault();
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then (res => {
+                if (res.user) Auth.setLoggedIn(true);
+            })
+            .catch(e => {
+                setErrors(e.message);
+            });
+    };
+
+    return (
+        <div>
+            <h1>Join</h1>
+            <form onSubmit={e => handleForm(e)}>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                />
+                <input
+                    onChange={e => setPassword(e.target.value)}
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="password"
+                />
+                <hr />
+                <button class="googleBtn" type="button">
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commonst/5/53/Google_%22G%22_Logo.svg"
+                        alt="logo"
+                    />
+                    Join With Google
+                </button>
+
+                <button type="submit">Login</button>
+
+                <span>{error}</span>
+            </form>
+        </div>
+    );
+}
+
+export default Join;
