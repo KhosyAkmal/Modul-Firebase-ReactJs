ca9d3f323fcec69733e831c59415b327a762680b
KhosyAkmal <akmalkhosy@gmail.com>
2020-04-16 13:45:34 +0700

    add firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyD3SEtzKc6aMnEvJs2gxCfWElMVpgNQbRc",
+    authDomain: "fir-reactjs-785d7.firebaseapp.com",
+    databaseURL: "https://fir-reactjs-785d7.firebaseio.com",
+    projectId: "fir-reactjs-785d7",
+    storageBucket: "fir-reactjs-785d7.appspot.com",
+    messagingSenderId: "174337766493",
+    appId: "1:174337766493:web:9c69a0bfc9d448206a89b4",
+    measurementId: "G-KHZG3NXQPG"
+  };
